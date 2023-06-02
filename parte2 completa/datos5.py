3301e29cbafb47b54d293af38d2bb541b83db5e0
rogercriollo <rlcriollo@utpl.edu.ec>
2023-06-02 11:32:38 -0500

    cambiosfinales

--- parte2 completa/datos5.py
+++ parte2 completa/datos5.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import and_ , or_
-from genera_tablas import Institucion, Parroquia, Provincia, Canton, TiposSostenimiento, Distrito
+from genera_tablas import Institucion, Parroquia, Provincia, Canton
 from configuracion import BaseDatos
 
 #Conexión con SQlite
@@ -11,11 +11,11 @@ Session = sessionmaker(bind=engine)
 with Session() as session:
     print("/********************************* NUmero de estudiantes *****************************/")
     # Los establecimientos ordenados por número de estudiantes; que tengan más de 100 profesores.
-    inst = session.query(Institucion).filter(Institucion.num_doc>100).order_by(Institucion.num_est).all()
+    inst = session.query(Institucion).filter(Institucion.numStudents>100).order_by(Institucion.numStudents).all()
     print(inst)
 
 
     print("/********************************* NUmero de Profesores *******************************/")
     # Los establecimientos ordenados por número de profesores; que tengan más de 100 profesores.
-    inst = session.query(Institucion).filter(Institucion.num_doc>100).order_by(Institucion.num_doc).all()
+    inst = session.query(Institucion).filter(Institucion.numTeachers>100).order_by(Institucion.numTeachers).all()
     print(inst)
