270751c9f8ac4eeff6a5f4d517a2bcdfc001a22b
rogercriollo <rlcriollo@utpl.edu.ec>
2023-06-02 12:47:39 -0500

    cambios

--- configuracion.py
+++ configuracion.py
@@ -1,11 +1,4 @@
 # Sqlite
-BaseDatos = 'sqlite:///final1bimaa22.db' 
-
-# Mysql
-# para el uso de este dialecto en SqlAlchemy se crea varibles
-host = 'localhost'
-port = '3306'
-username = 'root'
-password = ''
-database = 'final1bimaa22'
-#BaseDatos = f'mysql://{username}:{password}@{host}:{port}/{database}'
+##BaseDatos = 'sqlite:///final1bimaa22.db' 
+# instalar "pip install PyMySQL"
+BaseDatos = 'mysql+pymysql://root:2001@localhost/final'
